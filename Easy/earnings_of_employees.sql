eb8a007f1f6301b386d2c49566782450d96c3a92
kaczifant <35376563+kaczifant@users.noreply.github.com>
2019-11-03 20:07:23 +0100

    Add files via upload

---
+++ Easy/earnings_of_employees.sql
@@ -0,0 +1,10 @@
+/* https://www.hackerrank.com/challenges/earnings-of-employees/problem
+
+We define an employee's total earnings to be their monthly salary * months worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as 2 space-separated integers.*/
+
+SELECT salary * months AS max_total_earning, COUNT(*) AS number_of_employee
+FROM employee
+GROUP BY max_total_earning
+HAVING max_total_earning = MAX(salary * months)
+ORDER BY max_total_earning DESC
+LIMIT 1;
