1e399080e114ef51002887f6d5e360e4109876ed
kaczifant <35376563+kaczifant@users.noreply.github.com>
2019-11-03 17:44:54 +0100

    Add files via upload

---
+++ Easy/population_density_difference.sql
@@ -0,0 +1,6 @@
+-- https://www.hackerrank.com/challenges/population-density-difference/problem?h_r=profile
+
+/* Query the difference between the maximum and minimum populations in CITY.*/
+
+SELECT MAX(population) - MIN(population)
+FROM city;
